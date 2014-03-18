8895c54cbf8ea8ba78aafab53a5a0409ce2f1ba2
Julien Chaumond <chaumond@gmail.com>
2014-03-18 18:49:14 +0100

    Seed a client and a user
    run `php artisan db:seed`

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,35 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('OAuthClientsSeeder');
+		$this->call('OAuthUsersSeeder');
 	}
 
+}
+
+
+class OAuthClientsSeeder extends Seeder
+{
+	public function run()
+	{
+		DB::table('oauth_clients')->insert(array(
+			'client_id' => "testclient",
+			'client_secret' => "testpass",
+			'redirect_uri' => "http://fake/",
+		));
+	}
+}
+
+
+class OAuthUsersSeeder extends Seeder
+{
+	public function run()
+	{
+		DB::table('oauth_users')->insert(array(
+			'username' => "bshaffer",
+			'password' => "brent123",
+			'first_name' => "Brent",
+			'last_name' => "Shaffer",
+		));
+	}
 }
