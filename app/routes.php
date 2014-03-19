9e83fa2148321ba2a8c73baf6e54e08d02b5a7fd
Julien Chaumond <chaumond@gmail.com>
2014-03-19 15:40:49 +0100

    Retrive token data (including user id)

--- app/routes.php
+++ app/routes.php
@@ -47,8 +47,14 @@ Route::get('private', function()
 	$bridgedResponse = new OAuth2\HttpFoundationBridge\Response();
 	
 	if (App::make('oauth2')->verifyResourceRequest($bridgedRequest, $bridgedResponse)) {
+		
+		$token = App::make('oauth2')->getAccessTokenData($bridgedRequest);
+		
 		return Response::json(array(
-			'private' => 'stuff'
+			'private' => 'stuff',
+			'user_id' => $token['user_id'],
+			'client'  => $token['client_id'],
+			'expires' => $token['expires'],
 		));
 	}
 	else {
